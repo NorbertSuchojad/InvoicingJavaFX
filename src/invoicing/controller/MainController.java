d93b3e49f9fcbcdd2b106956dca47766d6240da0
norbert <norbert@ITflow>
2018-02-10 19:47:07 +0100

    add controllers and views

---
+++ src/invoicing/controller/MainController.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package invoicing.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * @author norbert
+ *
+ */
+
+public class MainController {
+
+    @FXML
+    private Button btnMyCompanyData;
+
+    @FXML
+    private Button btnOtherCompanyData;
+
+    @FXML
+    private Button btnNewInvoice;
+
+    @FXML
+    private Button btnShowInvoicing;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    void exitAction(MouseEvent event) {
+    	
+    }
+
+    @FXML
+    void prepareNewInvoice(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showInvoices(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showMyCompanyData(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showOtherCompanyData(MouseEvent event) {
+
+    }
+
+}
+
---
+++ src/invoicing/controller/MyCompanyController.java
@@ -0,0 +1,68 @@
+
+/**
+ * 
+ */
+package invoicing.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * @author norbert
+ *
+ */
+
+public class MyCompanyController {
+
+    @FXML
+    private ComboBox<?> comboFirma;
+
+    @FXML
+    private TextField tfCompanyName;
+
+    @FXML
+    private TextField tfNIP;
+
+    @FXML
+    private TextField tfREGON;
+
+    @FXML
+    private TextField tfAdress;
+
+    @FXML
+    private TextField tfPostCode;
+
+    @FXML
+    private TextField tfPost;
+
+    @FXML
+    private TextField tfBankAccount;
+
+    @FXML
+    private Button btnSaveAndClose;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnSave1;
+
+    @FXML
+    void back(MouseEvent event) {
+
+    }
+
+    @FXML
+    void save(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveAndClose(MouseEvent event) {
+
+    }
+
+}
